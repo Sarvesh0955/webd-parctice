20dff8976be718c350cb1e411e9fe96104c20575
Sarvesh0955 <sarvesh0955@gmail.com>
2024-12-29 21:05:01 +0530

    completed bootcamp

---
+++ player/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ScoreBoard from "./ScoreBoard";
+
+function App() {
+  return (
+    <>
+      <ScoreBoard numPlayers={10} winScore={5} />
+    </>
+  );
+}
+
+export default App;
